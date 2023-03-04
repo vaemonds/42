60f0222292276b1caaac2d0c581b888c28897dfc
Vaemonds <vaemonds@student.21-school.ru>
2023-03-04 19:39:37 +0300

    test interface in public/index.php

--- pubic/index.php
+++ pubic/index.php
@@ -3,4 +3,14 @@ require_once "../vendor/autoload.php";
 
 $time_to_word = new \App\TimeToWordConverter();
 
-print_r($time_to_word->convert(3, 0));
+print_r($time_to_word->convert(7, 0));
+print_r($time_to_word->convert(7, 01));
+print_r($time_to_word->convert(7, 03));
+print_r($time_to_word->convert(7, 12));
+print_r($time_to_word->convert(7, 15));
+print_r($time_to_word->convert(7, 22));
+print_r($time_to_word->convert(7, 30));
+print_r($time_to_word->convert(7, 35));
+print_r($time_to_word->convert(7, 41));
+print_r($time_to_word->convert(7, 56));
+print_r($time_to_word->convert(7, 59));
